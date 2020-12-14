772b2f6b7c709fddbe8a834204cdbdb4b7bbd85e
Ekenayy <ekenayy@gmail.com>
2020-12-14 18:03:35 -0500

    colors chosen, buttons created, and 2 mantras seeded

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+let mantraArray = [
+    {name: "Lotus Sutra",
+    description: "For many East Asian Buddhists since early times, the Lotus Sutra contains the final teaching of the Buddha, complete and sufficient for salvation.",
+    video: 'https://www.youtube.com/watch?v=isXLBHCDo4Y&ab_channel=TINATurnerBlog',
+    website: 'https://www.lionsroar.com/what-happens-in-the-lotus-sutra-unpacking-the-events-in-this-famous-buddhist-scripture/'
+    }, 
+
+    {
+     name: 'Heart Sutra',
+     description: 'The Heart Sutra is the single most commonly recited, copied and studied scripture in East Asian Buddhism.',
+     video: 'https://www.youtube.com/watch?v=VYOyDntRjDY&ab_channel=89992',
+     website: 'https://www.lionsroar.com/the-heart-sutra-will-change-you-forever/'
+    } 
+]
+
 // function toggleColor (element) {
 
 // }
